aff6c2275ded9e2ed33f49f2b0c5342248de4e04
ken <c.uvsuv@163.com>
2020-03-31 15:27:49 +0800

    添加少量代码

--- app/src/main/java/com/show/wanandroid/ui/main/fragment/CollectFragment.kt
+++ app/src/main/java/com/show/wanandroid/ui/main/fragment/CollectFragment.kt
@@ -1,14 +1,33 @@
 package com.show.wanandroid.ui.main.fragment
 
+import android.graphics.Color
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.show.wanandroid.R
 import com.show.wanandroid.databinding.FragmentCollectBinding
+import com.show.wanandroid.entity.Collect
+import com.show.wanandroid.themes_name
+import com.show.wanandroid.ui.main.adapter.CollectAdapter
 import com.show.wanandroid.ui.main.vm.MainViewModel
+import com.showmethe.skinlib.SkinManager
 import kotlinx.android.synthetic.main.fragment_collect.*
+import showmethe.github.core.adapter.slideAdapter.SlideCreator
 import showmethe.github.core.base.BaseFragment
+import showmethe.github.core.divider.RecycleViewDivider
+import showmethe.github.core.util.extras.ObList
+import showmethe.github.core.util.extras.plus
+import showmethe.github.core.util.extras.set
 import showmethe.github.core.util.widget.StatusBarUtil.fixToolbar
 
 class CollectFragment : BaseFragment<FragmentCollectBinding, MainViewModel>() {
+
+    private val pager = MutableLiveData<Int>()
+    private lateinit var adapter: CollectAdapter
+    private val list = ObList<Collect.DatasBean>()
 
     override fun initViewModel(): MainViewModel = createViewModel()
 
@@ -18,21 +37,53 @@ class CollectFragment : BaseFragment<FragmentCollectBinding, MainViewModel>() {
     }
 
     override fun observerUI() {
+
+        pager.observe(this, Observer {
+            it?.apply {
+                router.toTarget("getCollect", this)
+            }
+        })
+
     }
 
     override fun init(savedInstanceState: Bundle?) {
         fixToolbar(toolBar)
         binding?.main = this
 
+        initAdapter()
+
+
+        pager set 0
     }
 
     override fun initListener() {
 
+        rv.setOnLoadMoreListener {
+            pager.plus(1)
+        }
+
+        refresh.setOnRefreshListener {
+            pager set 0
+        }
 
     }
 
+    private fun initAdapter() {
+        adapter = CollectAdapter(context, list)
+        rv.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rv.adapter = adapter
+        rv.hideWhenScrolling(refresh)
+        val color = when (SkinManager.currentStyle) {
+            themes_name[0] -> R.color.colorAccent
+            themes_name[1] -> R.color.color_304ffe
+            themes_name[2] -> R.color.color_6200ea
+            else -> R.color.colorAccent
+        }
+        SlideCreator().addItemMenu(getString(R.string.cancel_collect), R.color.white, color, 15f)
+            .bind(adapter, 0.2f)
+    }
 
-    fun backPress(){
+    fun backPress() {
         requireActivity()
             .supportFragmentManager.popBackStack()
     }
