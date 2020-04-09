f3a19d401f40dd6d6ef4407c7a2a0e6d64320f09
ken <c.uvsuv@163.com>
2020-04-09 23:46:09 +0800

    添加代码

--- app/src/main/java/com/show/wanandroid/util/DexToFlutter.kt
+++ app/src/main/java/com/show/wanandroid/util/DexToFlutter.kt
@@ -1,6 +1,9 @@
 package com.show.wanandroid.util
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
+import dalvik.system.DexClassLoader
 import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileOutputStream
@@ -12,11 +15,11 @@ object DexToFlutter {
         val cacheDir = context.externalCacheDir!!.path
         val flutterApk = File("$cacheDir/flutter.apk")
         if(flutterApk.exists()){
-
+            dexApk(context,flutterApk)
         }else{
             val ins = context.assets.open("flutter.apk")
             val out = BufferedOutputStream(FileOutputStream(flutterApk.path))
-            var buf = ByteArray(4096)
+            val buf = ByteArray(4096)
             var len = 0
             while((ins.read(buf).also { len =  it})!=-1){
                 out.write(buf,0,len)
@@ -24,7 +27,15 @@ object DexToFlutter {
             out.flush()
             out.close()
             ins.close()
+            dexApk(context,flutterApk)
         }
+
+    }
+
+    private fun dexApk(context: Context,file: File) {
+        val dexClassLoader = DexClassLoader(file.path,context.externalCacheDir!!.path,null,
+            ClassLoader.getSystemClassLoader())
+        val clazz = dexClassLoader.loadClass("com.ken.androidflutter.MainActivity")
 
     }
 
